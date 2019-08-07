dfef5d72afdb8a7233dfd63cfb7a2f22ba600c44
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-08-08 08:05:06 +1000

    Added image and debug text for scroll example

--- StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
+++ StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
@@ -11,12 +11,38 @@ import SwiftUI
 struct ScrollViewExample: View {
     var body: some View {
         ScrollView {
-            Text("Row 1")
-            Text("Row 2")
-            Text("Row 3")
-            Text("Row 4")
-            Text("Row 5")
-            Text("Row 6")
+            GeometryReader { reader -> AnyView in
+                let frame = reader.frame(in: .global)
+                let height = frame.maxY
+                return AnyView(
+                    ZStack {
+                        Image("willian-justen-de-vasconcellos")
+                            .frame(height: height, alignment: .top)
+                            .clipped()
+                        VStack(spacing: 4) {
+                            Text("Willian\nJusten de Vasconcellos")
+                                .foregroundColor(.white)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .font(.title)
+                            Text("debugFrame: \(frame.debugDescription)")
+                            Text("maxY: \(frame.maxY)")
+                        }
+                    }
+                    .frame(width: reader.size.width)
+                )
+            }
+            .frame(height: 400)
+            
+            Group {
+                Text("Row 1")
+                Text("Row 2")
+                Text("Row 3")
+                Text("Row 4")
+                Text("Row 5")
+                Text("Row 6")
+            }
+            .offset(y: 44)
         }
     }
 }
