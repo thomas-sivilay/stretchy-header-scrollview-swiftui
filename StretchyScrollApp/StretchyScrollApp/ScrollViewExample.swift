5310221c8d294f3ac595bd9451f3be7dadf7afef
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-08-08 08:31:03 +1000

    Tweak

--- StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
+++ StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
@@ -9,40 +9,47 @@
 import SwiftUI
 
 struct ScrollViewExample: View {
+    
+    @State private var maxY: CGFloat = 0
+    var threshold: CGFloat = 400
+    
     var body: some View {
         ScrollView {
-            GeometryReader { reader -> AnyView in
-                let frame = reader.frame(in: .global)
-                let height = frame.maxY
-                return AnyView(
-                    ZStack {
-                        Image("willian-justen-de-vasconcellos")
-                            .frame(height: height, alignment: .top)
-                            .clipped()
-                        VStack(spacing: 4) {
-                            Text("Willian\nJusten de Vasconcellos")
-                                .foregroundColor(.white)
-                                .bold()
-                                .multilineTextAlignment(.center)
-                                .font(.title)
-                            Text("debugFrame: \(frame.debugDescription)")
-                            Text("maxY: \(frame.maxY)")
-                        }
-                    }
-                    .frame(width: reader.size.width)
-                )
-            }
-            .frame(height: 400)
-            
             Group {
-                Text("Row 1")
-                Text("Row 2")
-                Text("Row 3")
-                Text("Row 4")
-                Text("Row 5")
-                Text("Row 6")
+                GeometryReader { reader -> AnyView in
+                    let frame = reader.frame(in: .global)
+                    let height = frame.maxY
+                    self.maxY = height
+                    return AnyView(
+                        ZStack {
+                            Image("willian-justen-de-vasconcellos")
+                                .fixedSize(horizontal: true, vertical: false)
+                                .frame(height: height)
+                            VStack(spacing: 4) {
+                                Text("Willian\nJusten de Vasconcellos")
+                                    .foregroundColor(.white)
+                                    .bold()
+                                    .multilineTextAlignment(.center)
+                                    .font(.title)
+                                Text("debugFrame: \(frame.debugDescription)")
+                                Text("maxY: \(self.maxY)")
+                            }
+                        }
+                        .frame(width: reader.size.width)
+                    )
+                }
             }
-            .offset(y: 44)
+            .frame(height:
+                maxY <= 400 ? 400 : min(maxY, 400)
+            )
+            .clipped()
+            
+            Text("Row 1")
+            Text("Row 2")
+            Text("Row 3")
+            Text("Row 4")
+            Text("Row 5")
+            Text("Row 6")
         }
     }
 }
