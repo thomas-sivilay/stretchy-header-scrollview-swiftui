d47006e75950d6a503dba2ffc1552ca3092f0f69
Thomas Sivilay <thomas.sivilay@gmail.com>
2019-08-15 07:26:26 +1000

    Clean up

--- StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
+++ StretchyScrollApp/StretchyScrollApp/ScrollViewExample.swift
@@ -10,44 +10,45 @@ import SwiftUI
 
 struct ScrollViewExample: View {
     
-    @State private var offset: CGFloat = 0
+    @State private var offset: CGFloat = 100
     
     var body: some View {
         ZStack {
             ScrollViewHeaderView(offset: $offset)
-            ScrollViewContentView()
-                .offset(x: 0, y: 220 + offset)
-                .padding(.bottom, 220 + offset)
+                .zIndex(10)
+            Group {
+                ScrollViewContentView(offset: $offset)
+                    .padding(.bottom, 0)
+                    .zIndex(1)
+            }
+            .offset(x: 0, y: 0)
         }
     }
 }
 
 struct ScrollViewHeaderView: View {
     
+    let minHeight: CGFloat = 0
     @Binding var offset: CGFloat
     
     var body: some View {
         GeometryReader { proxy -> AnyView in
-            let frame = proxy.frame(in: .global)
-            let height = min(frame.maxY, 400)
-
             return AnyView(
                 ZStack {
-                    Image("willian-justen-de-vasconcellos")
-                        .fixedSize(horizontal: true, vertical: false)
-                        .frame(height: 400)
+                    Image("catalina")
+                        .scaleEffect(0.25 + (self.offset / 600))
+                        .frame(height: max(self.offset, self.minHeight))
                         .clipped()
                     VStack(spacing: 4) {
-                        Text("Willian\nJusten de Vasconcellos")
+                        Text("Content offset: \(self.offset)")
                             .foregroundColor(.white)
                             .bold()
                             .multilineTextAlignment(.center)
                             .font(.title)
-                        Text("debugFrame: \(frame.debugDescription)")
-                        Text("maxY: \(self.offset)")
                     }
                 }
-                .frame(width: proxy.size.width, height: 300)
+                .frame(width: proxy.size.width, height: max(self.offset, self.minHeight))
+                .clipped()
             )
         }
     }
@@ -63,21 +64,21 @@ struct ScrollOffsetPreferenceKey: PreferenceKey, Equatable {
 
 struct ScrollViewContentView: View {
     
+    let correction: CGFloat = 85
+    @Binding var offset: CGFloat
+    
     var body: some View {
         GeometryReader { proxy -> AnyView in
-            print(proxy)
-            
             return AnyView(
                 Form {
                     GeometryReader { epProxy -> AnyView in
                         return AnyView(
-                            Text("\(epProxy.frame(in: .global).minY)")
+                            Text("\(epProxy.frame(in: .named("myZstack")).minY - self.correction)")
                                 .preference(key: ScrollOffsetPreferenceKey.self,
-                                            value: [epProxy.frame(in: .global).minY])
+                                            value: [epProxy.frame(in: .named("myZstack")).minY - self.correction])
                         )
                     }
                         
-                    
                     Section(header: Text("0")) {
                         NavigationLink(destination: DetailView(title: "Nav")) {
                             Text("Navigation link")
@@ -111,11 +112,12 @@ struct ScrollViewContentView: View {
                     }
 
                 }
-                .frame(width: proxy.size.width)
-                .coordinateSpace(name: "myZstack")
                 .onPreferenceChange(ScrollOffsetPreferenceKey.self) {
                     print($0)
+                    self.offset = $0.first ?? 0
                 }
+                .frame(width: proxy.size.width)
+                .coordinateSpace(name: "myZstack")
             )
         }
     }
